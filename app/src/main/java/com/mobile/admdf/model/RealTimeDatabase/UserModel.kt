0843c9087ba2ad1bf3003a5fa25fc25110428212
Makeavel <dpseutroco@gmail.com>
2021-03-29 23:43:37 -0300

    Melhorias

--- app/src/main/java/com/mobile/admdf/model/RealTimeDatabase/UserModel.kt
+++ app/src/main/java/com/mobile/admdf/model/RealTimeDatabase/UserModel.kt
@@ -3,7 +3,7 @@ package com.mobile.admdf.model.RealTimeDatabase
 class UserModel {
 
     val id : String? = null
-    val nome : String? = null
+    val nome : String? = null   //val nome = tv_nomeCad.text.toString()
     val email : String? = null
 
 
