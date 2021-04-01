9cc55a4b2fc35c1f1349d00910ec474cec7136dd
Makeavel <dpseutroco@gmail.com>
2021-03-31 23:32:27 -0300

    Adicionando fontes + telas

---
+++ app/src/main/java/com/mobile/admdf/ui/fragment/screen/FundoPretoFragment.kt
@@ -0,0 +1,20 @@
+package com.mobile.admdf.ui.fragment.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mobile.admdf.R
+
+class FundoPretoFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fundo_preto, container, false)
+    }
+
+
+}
